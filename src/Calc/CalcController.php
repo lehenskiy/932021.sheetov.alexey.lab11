d0bdce0d40c3fbd21422733ea63357b374eab81f
Alexey Sheetov <50871152+lehenskiy@users.noreply.github.com>
2023-12-02 01:29:43 +0700

    L11-3: Calc index (#3)
    * Add controller
    
    * Add service
    
    * Add Enum
    
    * Add view files
    
    * Add route in config

---
+++ src/Calc/CalcController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Calc;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CalcController extends AbstractController
+{
+    #[Route(path: '/calc/{mode}', name: 'show_calc')]
+    public function index(CalcEnum $mode, CalcService $calcService): Response
+    {
+        $firstValue = random_int(1, 100);
+        $secondValue = random_int(1, 100);
+
+        $calcDTO = $calcService->getCalcDTO($firstValue, $secondValue);
+
+        return $this->render('@src_dir/Calc/output.twig', [
+            'calcDTO' => $calcDTO,
+        ]);
+    }
+}
